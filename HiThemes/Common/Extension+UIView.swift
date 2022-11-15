991b4aadae68e25daa37a8d5b87a1058e77feb3a
HiepTQ5 <Hieptq5@fpt.com.vn>
2022-11-15 10:45:46 +0700

    add line

--- HiThemes/Common/Extension+UIView.swift
+++ HiThemes/Common/Extension+UIView.swift
@@ -43,4 +43,15 @@ extension UIView{
             }
         })
     }
+    func addLine(fromPoint start: CGPoint, toPoint end: CGPoint,color : UIColor) {
+        let line = CAShapeLayer()
+        let linePath = UIBezierPath()
+        linePath.move(to: start)
+        linePath.addLine(to: end)
+        line.path = linePath.cgPath
+        line.strokeColor = color.cgColor
+        line.lineWidth = 1
+        line.lineJoin = CAShapeLayerLineJoin.round
+        self.layer.addSublayer(line)
+    }
 }
